da5a94086575c54d7d682214da58a46b4bc2b48c
paulinespicher <pauline.spicher@studentfr.ch>
2025-05-13 10:52:29 +0200

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+var hand = document.querySelector(".hand");
+var box1 = document.querySelector(".box1");
+var box2 = document.querySelector(".box2");
+var box3 = document.querySelector(".box3");
+var box4 = document.querySelector(".box4");
+
+hand.addEventListener("click", function () {
+  box1.classList.add("is-active");
+  box2.classList.add("is-active");
+  box3.classList.add("is-active");
+  box4.classList.add("is-active");
+});
